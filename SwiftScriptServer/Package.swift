a9ca51faa4b0d6bda27fbcc98ae9e5653e7398af
Wataru Nishimoto <nishimoto.wataru@zaim.co.jp>
2017-03-04 13:16:07 +0900

    update dependencies

--- SwiftScriptServer/Package.swift
+++ SwiftScriptServer/Package.swift
@@ -1,5 +1,8 @@
 import PackageDescription
 
 let package = Package(
-    name: "SwiftScriptServer"
+    name: "SwiftScriptServer",
+    dependencies: [
+        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 6)
+    ]
 )
--- SwiftScriptServer/Sources/main.swift
+++ SwiftScriptServer/Sources/main.swift
@@ -1,1 +1,23 @@
-print("Hello, world!")
+import Foundation
+import Kitura
+
+let router = Router()
+
+// Handle HTTP GET requests to /
+router.get("/") { request, response, next in
+    response.send("try? Swift 2017")
+    next()
+}
+
+
+
+router.post("") { request, response, next in
+    response.send("try! Swift 2017 Post")
+    next()
+}
+
+// Add an HTTP server and connect it to the router
+Kitura.addHTTPServer(onPort: 8080, with: router)
+
+// Start the Kitura runloop (this call never returns)
+Kitura.run()
