9a4633a92643b96486a9984b6a6b9990991fe649
Ubuntu <ubuntu@ip-172-31-5-182.ap-northeast-1.compute.internal>
2017-03-04 08:23:42 +0000

    fix old id

--- public/clicks.js
+++ public/clicks.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 $('#button').on('click', function() {
 	console.log($("#inputfield").val());
     $.post( "/", $("#inputfield").val(), function(data) {
-    	$('#right').text(data);	
+    	$('#output').text(data);	
     });	
 });
 });
