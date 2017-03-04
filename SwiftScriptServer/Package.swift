2d438e81f8de92140c1deca8f36fdb64fe81cd66
Wataru Nishimoto <nishimoto.wataru@zaim.co.jp>
2017-03-04 14:51:08 +0900

    add SwiftScript to dependencies

--- SwiftScriptServer/Package.swift
+++ SwiftScriptServer/Package.swift
@@ -3,6 +3,7 @@ import PackageDescription
 let package = Package(
     name: "SwiftScriptServer",
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 6)
+        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 6),
+        .Package(url: "https://github.com/swift-script/swift-script.git", majorVersion: 0, minor: 0)
     ]
 )
