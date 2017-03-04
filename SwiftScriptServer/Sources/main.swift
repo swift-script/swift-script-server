bde8f613e979d9d481204fe100ce220bb0b79b52
Wataru Nishimoto <nishimoto.wataru@zaim.co.jp>
2017-03-04 14:51:46 +0900

    use transpile method

--- SwiftScriptServer/Sources/main.swift
+++ SwiftScriptServer/Sources/main.swift
@@ -1,5 +1,6 @@
 import Foundation
 import Kitura
+import SwiftScript
 
 let router = Router()
 
@@ -11,9 +12,23 @@ router.get("/") { request, response, next in
 
 
 
-router.post("") { request, response, next in
-    response.send("try! Swift 2017 Post")
+router.post("/") { request, response, next in
+    guard let value = try request.readString() else {
+        response.send(status: .badRequest).send("; needs body\n")
+        try response.end()
+        return
+    }
+
+    let message = try transpile(code: value)
+    response.send("Your Input was : \(value)\n")
+    response.send(message)
+    response.send("\n")
+    try response.end()
     next()
+}
+
+router.error { request, response, next in
+
 }
 
 // Add an HTTP server and connect it to the router
