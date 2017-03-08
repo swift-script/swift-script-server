1d613c9ef1d72348d2b0aefd79f19f4158a0edfc
Wataru NISHIMOTO <watura@g91.org>
2017-03-08 20:46:57 +0900

    compile swift code before transpile

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,42 +1,15 @@
 import Foundation
 import Kitura
-import KituraCORS
-import SwiftScript
 import HeliumLogger
 import LoggerAPI
 
-let router = Router()
-let options = Options()
-let cors = CORS(options: options)
-let logger = HeliumLogger(.entry)
-Log.logger = logger
-
-router.get("/", middleware: StaticFileServer())
-
-router.post("/", middleware: cors)
-router.post("/") { request, response, next in
-    guard let value = try request.readString() else {
-        response.send("could not parse body\n")
-        Log.error("request.body: \(request.body)")
-        try response.end()
-        return
-    }
-
-    Log.verbose("swift: `\(value)`")
-    do {
-        let message = try transpile(code: value)
-        Log.verbose("javascript: `\(message)`")
-
-        response.send(message)
-        response.send("\n")
-    } catch let e {
-        Log.error("`\(value)` -> `\(e)`")
-        response.send("\(e)")
-    }
-    try response.end()
-    next()
+do {
+    // HeliumLogger disables all buffering on stdout
+    HeliumLogger.use(.entry)
+    let controller = try SSController()
+    Kitura.addHTTPServer(onPort: controller.port, with: controller.router)
+    // Start Kitura-Starter server
+    Kitura.run()
+} catch let error {
+    Log.error(error.localizedDescription)
 }
-
-Kitura.addHTTPServer(onPort: 8080, with: router)
-
-Kitura.run()
