58ddec9b1dbea7c5d5ef998c82eb791785c3dedc
Wataru NISHIMOTO <watura@g91.org>
2017-04-30 19:36:41 +0900

    update packages

--- Package.swift
+++ Package.swift
@@ -3,9 +3,9 @@ import PackageDescription
 let package = Package(
     name: "SwiftScriptServer",
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 6),
-        .Package(url: "https://github.com/IBM-Swift/Kitura-CORS.git", majorVersion: 1, minor: 6),
-        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 6),
+        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 7),
+        .Package(url: "https://github.com/IBM-Swift/Kitura-CORS.git", majorVersion: 1, minor: 7),
+        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 7),
         .Package(url: "https://github.com/swift-script/swift-script.git", majorVersion: 0, minor: 0)
     ]
 )
